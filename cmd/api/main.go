0ff24ad20ea9b8f63cde96b8111b111fe078cc1e
christian <christianadickerson@protonmail.com>
2021-07-13 17:48:36 -0700

    Exported AppStatus global typing/Setup connection to a localhost for dev/Gave ability to open the PostgreSQL database

---
+++ cmd/api/main.go
@@ -0,0 +1,98 @@
+package mainhh
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+
+	"github.com/SorbetofBeef/go-movies-backend/models"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+}
+
+type AppStatus struct {
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
+}
+
+type application struct {
+	config config
+	models models.Models
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Server mode")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://me:p443ck1s2dg1@localhost/go_movies?sslmode=disable", "Postgres connection string")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	serve := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Println("Starting server on port", cfg.port)
+
+	err = serve.ListenAndServe()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
