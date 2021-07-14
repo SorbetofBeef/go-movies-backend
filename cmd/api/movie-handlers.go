0cc19648c084a80d80081a92932bf97e30036e8e
christian <christianadickerson@protonmail.com>
2021-07-13 17:51:08 -0700

    Added the functions to handle API requests from the React client to the PostgreSQL

---
+++ cmd/api/movie-handlers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// getOneMovie handles retrieving a single movie from the db
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Println(errors.New("invalid id parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	app.logger.Println("id is", id)
+	movie, err := app.models.DB.Get(id)
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+
+}
